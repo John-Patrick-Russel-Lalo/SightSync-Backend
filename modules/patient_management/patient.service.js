778733a2bba8a068ca48dfe74fee8559d1321652
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2026-07-28 23:08:52 +0800

    patient edit profile endpoints for user and admin with different feature

---
+++ modules/patient_management/patient.service.js
@@ -0,0 +1,31 @@
+
+
+export const phoneNumberValidator = async (req, res) => {
+  try {
+    
+    const { phoneNumber } = req.body;
+
+    if (!phoneNumber || phoneNumber.length < 7 || phoneNumber.length > 15) {
+      return res.status(400).json({
+        success: false,
+        message: "Phone number must be at least 7 and at most 15 digits",
+      });
+    }
+
+    const validPhoneRegex = /^[0-9]{11}$/;
+
+    if (!validPhoneRegex.test(phoneNumber)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid phone number",
+      });
+    }
+    
+    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+}
